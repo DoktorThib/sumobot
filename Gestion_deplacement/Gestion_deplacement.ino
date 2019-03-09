8fb2cda9d08bcbcdab1eaa957e3dff3a0f5f74a9
Doktor <thibault.doktor@gmail.com>
2019-03-09 15:13:02 +0100

    add some deplacement functions
    You can choose the speed

--- Gestion_deplacement/Gestion_deplacement.ino
+++ Gestion_deplacement/Gestion_deplacement.ino
@@ -5,23 +5,20 @@
   by Thibault Doktor & Anthony Delétang
  */
 #include "ESC.h"
-#define LED_PIN (13)                                      // Pin for the LED 
+
 #define SPEED_MIN (1156)                                  // Set the Minimum Speed in microseconds
 #define SPEED_MAX (1500)                                  // Set the Minimum Speed in microseconds
 
-ESC myESC1 (2, SPEED_MIN, SPEED_MAX, 1000);                 // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
-ESC myESC2 (3, SPEED_MIN, SPEED_MAX, 1000);                 // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
+ESC myESC1 (2, SPEED_MIN, SPEED_MAX, 1000);               // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
+ESC myESC2 (3, SPEED_MIN, SPEED_MAX, 1000);               // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
 
 int oESC;                                                 // Variable for the speed sent to the ESC
 int strat = 0;
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);                               // LED Visual Output
   myESC1.arm();                                            // Send the Arm value so the ESC will be ready to take commands
-  delay(500);
   myESC2.arm();                                            // Send the Arm value so the ESC will be ready to take commands
-  digitalWrite(LED_PIN, HIGH);                            // LED High Once Armed
-  delay(2000); 
+  delay(2000);
 }
 
 void loop() {
@@ -49,5 +46,24 @@ void rotateLeft(){
 void stopRobot(){
   myESC1.stop();
   myESC2.stop();
+  delay(20);
+}
+
+void speedForward(int speedPercent){    //Speed in percent 0 MIN and 100 MAX
+  int trueSpeed = SPEED_MIN + (speedPercent / 100)* (SPEED_MAX - SPEED_MIN);
+  myESC1.speed(trueSpeed);
+  myESC2.speed(trueSpeed);
+  delay(20);
+}
+
+void speedRotateRight(int speedPercent){    //Speed in percent 0 MIN and 100 MAX
+  int trueSpeed = SPEED_MIN + (speedPercent / 100)* (SPEED_MAX - SPEED_MIN);
+  myESC1.speed(trueSpeed);
+  delay(20);
+}
+
+void speedRotateLeft(int speedPercent){    //Speed in percent 0 MIN and 100 MAX
+  int trueSpeed = SPEED_MIN + (speedPercent / 100)* (SPEED_MAX - SPEED_MIN);
+  myESC2.speed(trueSpeed);
   delay(20);
 }
